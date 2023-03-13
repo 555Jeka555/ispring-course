b594635d627ed658cfd0739aa38bccb76f254b80
555Jeka555 <2003antyshev@mail.ru>
2023-03-13 19:20:33 +0300

    Edit laba1

--- laba1/1.php
+++ laba1/1.php
@@ -1,17 +1,16 @@
 <?php 
-    if ($argc <= 1)
+if ($argc <= 1)
+{
+    echo "Arguments: 0";
+    return;
+}
+$arr = array_slice($argv, 1);
+foreach ($arr as $v)
+{
+    if (!is_numeric($v))
     {
-        echo "Arguments: 0";
+        echo "Incorrect type, pleace, enter numbers";
         return;
-    }
-    $arr = array_slice($argv, 1);
-    foreach ($arr as $v)
-    {
-        if (!is_numeric($v))
-        {
-            echo "Incorrect type, pleace, enter numbers";
-            return;
-        } 
-    }
-    echo "Min:", min($arr), ", max:", max($arr)
-;
+    } 
+}
+echo "Min:", min($arr), ", max:", max($arr);
--- laba1/2.php
+++ laba1/2.php
@@ -1,32 +1,31 @@
 <?php
-    if ($argc <= 1)
+if ($argc <= 1)
+{
+    echo "Arguments: 0";
+    return;
+}
+
+$value = preg_split("/=/", $argv[1])[0];
+$sMax = $value;
+$sMin = $value;
+
+for ($i = 2; $i < $argc; $i++)
+{
+    $lenMax = strlen($sMax);
+    $lenMin = strlen($sMin);
+
+    $value = preg_split("/=/", $argv[$i])[0];
+    $lenvalue = strlen($value);
+
+    if (strncmp($sMax, $value, min($lenMax, $lenvalue)) === -1)
     {
-        echo "Arguments: 0";
-        return;
-    }
+        $sMax = $value;
+    } 
 
-    $value = preg_split("/=/", $argv[1])[0];
-    $sMax = $value;
-    $sMin = $value;
-
-    for ($i = 2; $i < $argc; $i++)
+    if (strncmp($sMin, $value, min($lenMin, $lenvalue)) === 1)
     {
-        $lenMax = strlen($sMax);
-        $lenMin = strlen($sMin);
-
-        $value = preg_split("/=/", $argv[$i])[0];
-        $lenvalue = strlen($value);
-
-        if (strncmp($sMax, $value, min($lenMax, $lenvalue)) === -1)
-        {
-            $sMax = $value;
-        } 
-
-        if (strncmp($sMin, $value, min($lenMin, $lenvalue)) === 1)
-        {
-            $sMin = $value;
-        }
+        $sMin = $value;
     }
+}
 
-    echo "Min: ", $sMin, " max: ", $sMax;
-;
+echo "Min: ", $sMin, " max: ", $sMax;
